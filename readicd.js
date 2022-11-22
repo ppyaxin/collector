2d12228a14f3cba368ffbcaa1c4935d910b7a911
ccoffline <2385138002@qq.com>
2022-11-22 17:09:05 +0800

    mm

--- readicd.js
+++ readicd.js
@@ -20,7 +20,27 @@ function sendicd(v) {
 }
 function clickicdFile() {
     const input = document.querySelector("#new")
-    console.log("wwwwwww", input)
+    input.click()
+}
+
+
+function sendicdclass(v) {
+    console.log("icdicd", v)
+    let n = document.getElementById("icdclass").files.item(0)
+    let m = window.URL.createObjectURL(n)
+    $("#classLoc").textbox('setValue', m)
+    $('#openclass').form({
+        url: "readxml.php",
+        success: function (data) {
+            //alert(data)
+            console.log('OOOOOOOO', data)
+            readclass(data)
+        }
+    })
+    $('#openclass').submit()
+}
+function clickicdclassFile() {
+    const input = document.querySelector("#icdclass")
     input.click()
 
 }
@@ -40,11 +60,25 @@ function readicd(data) {
     })
 }
 
+//将xml存进服务器之后，读取再修改
+function readclass(data) {
+    $.ajax({
+        type: 'GET',
+        url: data,
+        dataType: 'xml',
+        success: function (x) {
+            // console.log("icdicd",icd)
+            icd = x
+            makeVirtualDom(icd)
+            return icd
+        }
+    })
+}
 
 function makeVirtualDom(icd) {
-    let A429portDom = icd.getElementsByTagName("A429Port")
+    //let A429portDom = icd.getElementsByTagName("A429Port")
     //console.log(A429portDom)
-    getA429portDom(A429portDom)
+    // getA429portDom(A429portDom)
     let DP = icd.getElementsByTagName("DP")
     getDPDom(DP)
 }
@@ -62,16 +96,66 @@ function getA429portDom(A429portDom) {
     }
 }
 
-let dptree=[]
-
+let dptree = []
+let checkedNode = []
 function getDPDom(DP) {
-    for(let i=0;i<DP.length;i++){
-        let obj={}
-        let path=DP[i].attributes[2].value.split('.')
-        mergeTree(dptree, path)
+    //console.log(dp)
+    for (let i = 0; i < DP.length; i++) {
+        let path = DP[i].getAttribute("NameDef")
+        let nodes = path.split(".")
+        mergeTreeAndLeaf(dptree, nodes, path)
     }
     console.log(dptree)
     $("#icdlist").tree({ data: dptree })
+    $("#icdlist").tree({
+        onCheck: function (node, checked) {
+            console.log("node", node, checked)
+            if(checked){
+                checkedNode=node
+            }
+            let nodes = $('#icdlist').tree('getChecked');
+            let leafs = []
+            for (let i = 0; i < nodes.length; i++) {
+                if (nodes[i].children.length == 0) {
+                    leafs.push({ text: nodes[i].path })
+                }
+            }
+            // console.log(leafs)
+            $("#iims").datalist({ data: leafs })
+        }
+    })
 }
 
+function puticd() {
+   // console.log("hhhhhhhhh",checkedNode)
+    $('#w').window('close')
+    let data=[]
+    data.push(checkedNode)
+    $("#omslist").tree({ data: data })
+}
 
+//nameref树,tree是js对象数组
+function mergeTreeAndLeaf(tree, nameNode, path) {
+    if (nameNode.length == 0) {
+        return
+    }
+    //如果这一层已经这个节点值了
+    for (let i = 0; i < tree.length; i++) {
+        if (tree[i].text == nameNode[0]) {
+            mergeTreeAndLeaf(tree[i].children, nameNode.slice(1), path)
+            return
+        }
+    }
+    if (nameNode.length == 2) {
+        tree.push({ text: nameNode[0], children: [], state: "closed", path: path })
+    } else {
+        tree.push({ text: nameNode[0], children: [], path: path })
+    }
+    mergeTreeAndLeaf(tree[tree.length - 1].children, nameNode.slice(1), path)
+    return
+}
+
+function removeit(){
+    var node = $('#omslist').tree('getSelected');
+    $('#omslist').tree('remove', node.target);
+}
