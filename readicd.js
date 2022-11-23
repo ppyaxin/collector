71c429b8ecc5e57f4b7d29642d34c8a908b5bac2
ccoffline <2385138002@qq.com>
2022-11-23 19:10:42 +0800

    ooo

--- readicd.js
+++ readicd.js
@@ -6,6 +6,7 @@ function sendicd(v) {
     let n = document.getElementById("new").files.item(0)
     let m = window.URL.createObjectURL(n)
     $("#InstanceLoc").textbox('setValue', m)
+    $("#createtime").textbox('setValue', getDay())
     //console.log("pppppp",window.URL.createObjectURL(n))
     //console.log("UUU",v.value)
     $('#openicd').form({
@@ -23,12 +24,25 @@ function clickicdFile() {
     input.click()
 }
 
+function getDay() {
+    let date = new Date()
+    let nowMonth = date.getMonth() + 1
+    let strDate = date.getDate()
+    if (nowMonth >= 1 && nowMonth <= 9) {
+        nowMonth = "0" + nowMonth
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate
+    }
+    return date.getFullYear() + "/" + nowMonth + "/" + strDate
+}
 
 function sendicdclass(v) {
     console.log("icdicd", v)
     let n = document.getElementById("icdclass").files.item(0)
     let m = window.URL.createObjectURL(n)
     $("#classLoc").textbox('setValue', m)
+    $("#createtime").textbox('setValue', getDay())
     $('#openclass').form({
         url: "readxml.php",
         success: function (data) {
@@ -79,6 +93,9 @@ function makeVirtualDom(icd) {
     //let A429portDom = icd.getElementsByTagName("A429Port")
     //console.log(A429portDom)
     // getA429portDom(A429portDom)
+    let HostedFunction = icd.getElementsByTagName("HostedFunction")[0]
+    let version = HostedFunction.getAttribute("XsdVersion")
+    $("#version").textbox('setValue', version)
     let DP = icd.getElementsByTagName("DP")
     getDPDom(DP)
 }
@@ -109,29 +126,33 @@ function getDPDom(DP) {
     $("#icdlist").tree({ data: dptree })
     $("#icdlist").tree({
         onCheck: function (node, checked) {
-            console.log("node", node, checked)
-            if(checked){
-                checkedNode=node
-            }
+            // console.log("node", node, checked)
+            // if(checked){
+            //     checkedNode=node
+            // }
             let nodes = $('#icdlist').tree('getChecked');
             let leafs = []
             for (let i = 0; i < nodes.length; i++) {
                 if (nodes[i].children.length == 0) {
                     leafs.push({ text: nodes[i].path })
                 }
+                //组成新的子树
+                mergeTree(checkedNode, nodes[i].path.split("."))
             }
             // console.log(leafs)
             $("#iims").datalist({ data: leafs })
+
+
         }
     })
 }
 
 function puticd() {
-   // console.log("hhhhhhhhh",checkedNode)
+    console.log("hhhhhhhhh", checkedNode)
     $('#w').window('close')
-    let data=[]
-    data.push(checkedNode)
-    $("#omslist").tree({ data: data })
+    //let data=[]
+    // data.push(checkedNode)
+    $("#omslist").tree({ data: checkedNode })
 }
 
 //nameref树,tree是js对象数组
@@ -155,7 +176,34 @@ function mergeTreeAndLeaf(tree, nameNode, path) {
     return
 }
 
-function removeit(){
+function removeit() {
     var node = $('#omslist').tree('getSelected');
     $('#omslist').tree('remove', node.target);
+}
+
+
+function icdsubmit() {
+    left_tt = [{
+        "id": 1,
+        "text": "Start Page"
+    }, {
+        "id": 2,
+        "text": "ICD Information",
+        "children": [
+            {
+                "id": 11,
+                "text": "Member System Modeling Data"
+            }]
+    }]
+    $("#tt").tree({ data: left_tt })
+}
+
+function msmdsubmit() {
+    console.log("ppppppppppppp")
+    let a = document.getElementById("supportFR").checked
+    console.log(a)
+    if(a){
+        left_tt[1].children.push({"text": "OMS Communication Messages"})
+        $("#tt").tree({ data: left_tt })
+    }
 }
