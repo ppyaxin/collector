146828b53a7332bd88dab279e1d9749d8a474748
pp <pyx518@qq.com>
2023-01-11 17:00:10 +0800

    oo

--- changeFaultReport.js
+++ changeFaultReport.js
@@ -14,16 +14,19 @@ function saveFaultReport() {
     //console.log("msd,msd", msd)
     //获取到 faultreport
     let FaultReportingData = msd.getElementsByTagName("FaultReportingData")
-   
+
     for (let i = 0; i < FaultReportingData.length; i++) {
         if (changedFaultReportId == FaultReportingData[i].getAttribute("Id")) {
             //tab1 basic data的数据
             let faultReportDelay = $("#Delay").val()
             let timeDelayDetect = $("#Detect").val()
             let timeDelayClear = $("#Clear").val()
+            let protocalType = $("protocalType").val()
             FaultReportingData[i].setAttribute("TimeDelayDetect", timeDelayDetect)
             FaultReportingData[i].setAttribute("TimeDelayClear", timeDelayClear)
             FaultReportingData[i].setAttribute("FaultReportDelay", faultReportDelay)
+            FaultReportingData[i].setAttribute("FaultReportType", protocalType)
+
             //tab2 storeable fight phase的数据
             //获取页面上被选到的check选项的编号
             let checkedItem = $("#fightPhase").tree('getChecked')
@@ -44,12 +47,23 @@ function saveFaultReport() {
             for (let j = 0; j < checkedItem.length; j++) {
                 let StorableFlightPhasesActiveFault = msd.createElement("StorableFlightPhasesActiveFault")
                 let context = msd.createTextNode(checkedItem[j].id)
-               
+
                 StorableFlightPhasesActiveFault.appendChild(context)
                 FaultReportingData[i].appendChild(StorableFlightPhasesActiveFault)
-                 //加入换行的空白元素
-                 //let blankText = msd.createTextNode("\n")
-                 //FaultReportingData[i].appendChild(blankText)
+                //加入换行的空白元素
+                //let blankText = msd.createTextNode("\n")
+                //FaultReportingData[i].appendChild(blankText)
+            }
+
+            //tab3 FDE的数据绑定
+            let checkedFDE = $("#fde").datalist('getChecked')
+            console.log("checkedfde", checkedFDE)
+            //在父节点中加入绑定的fed的序号
+            for (let j = 0; j < checkedFDE.length; j++) {
+               let CorrelatedFDEFaultCode = msd.createElement("CorrelatedFDEFaultCode")
+               let context=msd.createTextNode(checkedFDE[j].number)
+               CorrelatedFDEFaultCode.appendChild(context)
+               FaultReportingData[i].appendChild(CorrelatedFDEFaultCode)
             }
             console.log("FaultReportingData[i]", FaultReportingData[i])
 
@@ -64,6 +78,8 @@ function saveFaultReport() {
     $('#changeFault').window('close')
     console.log(" $('#changeFault').window('close')")
 }
+
+
 
 function saveDownStreamFaultReport() {
     if (changedFaultReportId.length == 0) {
@@ -101,7 +117,7 @@ function saveDownStreamFaultReport() {
                 FaultReportingData[i].appendChild(downfault)
             }
             getFaultReporting(FaultReportingData)
-           // console.log("FaultReportingData[i]", FaultReportingData[i])
+            // console.log("FaultReportingData[i]", FaultReportingData[i])
             break
         }
     }
@@ -112,12 +128,12 @@ function deleteBlank(parent) {
     //进入方法
     console.log("进入方法")
     let nodes = parent.childNodes
-    
+
     for (let i = nodes.length - 1; i > 0; i--) {
         //console.log(nodes[i].nodeType == 3)
         //console.log(/^\s+$/.test(nodes[i].nodeValue))
         if (nodes[i].nodeType == 3 && /^\s+$/.test(nodes[i].nodeValue)) {
-              //blankNode=nodes[i]
+            //blankNode=nodes[i]
             if (nodes[i - 1].nodeType == 3 && /^\s+$/.test(nodes[i - 1].nodeValue)) {
                 console.log(nodes[i])
                 parent.removeChild(nodes[i])
